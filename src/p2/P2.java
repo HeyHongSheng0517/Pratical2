411387f6ec11f1c92080dbf88d23ca5dbfc24520
taruc <taruc@B9>
2017-10-23 12:58:19 +0800

    My first local Commit

--- src/p2/P2.java
+++ src/p2/P2.java
@@ -17,6 +17,8 @@ public class P2 {
     public static void main(String[] args) {
        System.out.println("Hello World");
        System.out.println("Jietan");
+        System.out.println("Jietan"); System.out.println("Jietan");
+       
     }
     
 }
